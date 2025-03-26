13585a273417a9038a08e75dc7290d22b87a3d17
superJaba 2220 <superjaba2000@gmail.com>
2025-03-26 22:23:45 +0300

    small fixes

--- bot.js
+++ bot.js
@@ -1,39 +1,44 @@
 import { Telegraf } from 'telegraf';
-import { message } from 'telegraf/filters';
-import { existsSync, read, readFileSync, writeFileSync, createReadStream } from 'fs';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import { readFile } from 'fs/promises';
 
 
 /////////
 
 
 const colorcodes = [37, 90, 31, 91, 92];
-function log(where, data, color=0) {
+
+function log(data, where='global', color=0) {
     process.stdout.write(`\x1b[${colorcodes[color]}m[${where}]: ${data}\x1b[0m\n`);
 };
 
 let broken = false;
-process.on('uncaughtException', error => {
+
+process.on('uncaughtException', err => {
     broken = true;
-    log('global', error, 3);
+    log(`Fatal global error! ${err}`, 'global', 3);
 });
+
+async function _readFile(path) {
+    return new Promise( res => {
+        readFile(path, { encoding: 'utf8' })
+            .then(res)
+            .catch(err => {
+                log(`Error reading the file! path: ${path} error: ${err}`, 'readFile()', 2);
+                return null;
+            });
+    });
+};
+
+const BOT_TOKEN = process.env.BOT_TOKEN || await _readFile('./BOT_TOKEN.txt') || '7818057731:AAEPt-Q-IFtGLgX-zpSvroPJoPtCHNVL1cA';
+
+if(!BOT_TOKEN) {
+    throw new Error('BOT_TOKEN not found in environment variables / local file');
+};
+
+const bot = new Telegraf(BOT_TOKEN);
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
-
-function readFile(path) {
-    try {
-        return readFileSync(path, { encoding: 'utf-8', flag: 'r' });
-    }catch(err){
-        log('readFile()', `Error reading the file! path: ${path} error: ${err}`, 2);
-    }
-};
-
-const API_KEY = process.env.BOT_TOKEN || readFile('./API_KEY.txt');
-const bot = new Telegraf(API_KEY);
 
 
 /////////
@@ -55,19 +60,19 @@ const members = [
     new ClubMember('Артём Мелконян', '@artem8274', ['🥵']),
     new ClubMember('Егор Сметанин', '@Goooldeeen', ['☠️']),
     new ClubMember('Джанер', '@zazer123', ['😮‍💨']),
-    new ClubMember('Никита Шатских', '', ['🫡']),
+    new ClubMember('Никита Шатских', '(в васапе сидит)', ['🫡']),
     new ClubMember('Демитрий Коняев', '@fiscif', ['🤙']),
     new ClubMember('Глебарик Мощный', '@icepeak66', ['😍'])
 ];
 
-async function checkChatMember(ctx) {
+/*async function checkChatMember(ctx) {
     const member = await ctx.telegram.getChatMember('@looksmaxing_club', ctx.chat.id);
     if (member.status != "member" && member.status != "administrator" && member.status != "creator"){
         return false;
     } else {
         return true;
     }
-};
+};*/
 
 
 /////////
@@ -85,7 +90,8 @@ async function sendPhoto(ctx, chat_id, source, isSpoiler=false) {
 }
 
 async function page_menu(ctx) {
-    await ctx.telegram.sendMessage(ctx.message?.chat?.id || ctx.update.callback_query.message.chat.id, 'Привет друг!\n\nПодпишись на канал @looksmaxing_club, если ещё не подписан!\n\nЛюксмакс тебе и твоему дому! 🤫', {
+    let chat_id = ctx.message?.chat?.id || ctx.update.callback_query.message.chat.id;
+    await ctx.telegram.sendMessage(chat_id, 'Привет друг!\n\nПодпишись на канал @looksmaxing_club, если ещё не подписан!\n\nЛюксмакс тебе и твоему дому! 🤫', {
         reply_markup: {
             inline_keyboard: [
                 [{text: 'Кто такие люксмаксы?', callback_data: 'whois'}, {text: 'Кто входит в люксмакс?', callback_data: 'clubmembers'}],
@@ -124,7 +130,7 @@ async function page_clubmembers(ctx) {
 
     let string = '<b>👇 Вот они все 👇</b>\n\n';
     for(let member of members) {
-        string += `${member.name} ${member.username}\n`;
+        string += `${member.emoji[0]} ${member.name} ${member.username}\n`;
     };
     string += '\n(Учи запоминай, повторяй перед сном)';
 
@@ -225,15 +231,17 @@ bot.on('callback_query', async ctx => {
 
 
 bot.on('message', async ctx => {
+    let chat_id = ctx.message?.chat?.id || ctx.update.message.chat.id;
     let text = ctx.message?.text || ctx.update.message?.text || '';
+    let raw_text = text.replace(/\s/g, '').toLowerCase();
 
-    if(text.replace(/\s/g, '').toLowerCase() == 'идинахуй') {
-        await ctx.telegram.sendMessage(ctx.message?.chat?.id || ctx.update.message.chat.id, 'сам сходи');
+    if(raw_text == 'идинахуй' || raw_text == 'пошёлнахуй' || raw_text == 'пошёлнахрен') {
+        await ctx.telegram.sendMessage(chat_id, 'сам сходи');
     }else if(text.replace(/\s/g, '')[0] == '/'){
-
+        // its command
     }else{
-        await ctx.telegram.sendMessage(ctx.message?.chat?.id || ctx.update.message.chat.id, 'еблан по русски скажи че те надо');
-        await ctx.telegram.sendMessage(ctx.message?.chat?.id || ctx.update.message.chat.id, 'нихуя не понимаю че ты высрал');
+        await ctx.telegram.sendMessage(chat_id, 'еблан по русски скажи че те надо');
+        await ctx.telegram.sendMessage(chat_id, 'нихуя не понимаю че ты высрал');
     }
 });
 
@@ -243,5 +251,6 @@ bot.on('message', async ctx => {
 
 bot.launch();
 
-if(!broken) 
-    log('global', 'the bot is running', 4);
+if(!broken) {
+    log('the bot is running', 'global', 4);
+} 
